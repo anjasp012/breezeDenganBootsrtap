2826eaf3416e599c1070bbe78334b8935f2aed14
anjas putra <anjas.2104@students.amikom.ac.id>
2021-11-24 18:21:07 +0700

    Update1 BreezeBootstrap

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-2">
     {{ $logo }}
 </div>
-<div class="card rounded-0">
+<div class="card" style="border-radius: 10px">
     <div class="card-body">
         {{ $slot }}
     </div>
